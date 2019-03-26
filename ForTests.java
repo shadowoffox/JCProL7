a1ed9eade1b8349c3aadeb9c5862b7e69f4d6bfa
shadow_of_fox <denisochnev@mail.ru>
2019-03-26 18:16:16 +0300

    add code

--- ForTests.java
+++ ForTests.java
@@ -1,14 +1,56 @@
 public class ForTests {
-
-
+String out;
     @BeforeSuite
-    public void B() { }
+    public void StartTest() {
+        System.out.println("DO IT!");
+    }
+
+    @Test(priority = Test.Priority.lowerst)
+    public void outIt1(){
+        System.out.println("out lowest");
+    }
+
+    @Test(priority = Test.Priority.very_low)
+    public void outIt2(){
+        System.out.println("out very_low");
+    }
+
+    @Test(priority = Test.Priority.lower)
+    public void outIt3(){
+        System.out.println("out lower");
+    }
+    @Test(priority = Test.Priority.low)
+    public void outIt4(){
+        System.out.println("out low");
+    }
 
     @Test()
-    public void outIt(){
-    System.out.println("out");
+    public void outIt5(){
+    System.out.println("out medium");
+    }
+
+    @Test(priority = Test.Priority.hight)
+    public void outIt6(){
+        System.out.println("out hight");
+    }
+
+    @Test(priority = Test.Priority.highter)
+    public void outIt7(){
+        System.out.println("out highter");
+    }
+
+    @Test(priority = Test.Priority.very_hight)
+    public void outIt8(){
+        System.out.println("out very_hight");
+    }
+
+    @Test(priority = Test.Priority.hightest)
+    public void outIt9(){
+        System.out.println("out hightest");
     }
 
     @AfterSuite
-      public void A(){ }
+      public void StopTest(){
+        System.out.println("STOP IT!");
+    }
 }
--- Test.java
+++ Test.java
@@ -6,5 +6,24 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD,ElementType.METHOD})
 public @interface Test {
-    String Type() default "AnnotTest";
+    enum Priority{
+   lowerst (1),
+   very_low (2),
+   lower (3),
+    low (4),
+    medium(5),
+    hight(6),
+    highter(7),
+   very_hight(8),
+    hightest(9);
+
+private int priority;
+
+    Priority(int priority){
+        this.priority=priority;
+    }
+    public int getPriority(){return priority;}
+
+}
+Priority priority() default Priority.medium;
 }
